9be60a428cfbef15115bcb434a299aa590afce6a
Chris Milson <chris.w.milson@gmail.com>
2021-02-01 17:51:17 +0900

    Rename jest typings

---
+++ src/typings/jest.d.ts
@@ -0,0 +1,12 @@
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      /**
+       * Checks whether or not the values obey the heap invariant according to
+       * the passed compare function.
+       */
+      toObeyHeapInvariant<T>(compare: (a: T, b: T) => number): R
+    }
+  }
+}
+export {}
