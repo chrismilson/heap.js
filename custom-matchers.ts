6ac76ec32d0d5631a7ac5cb91cc9648b306c23c4
Chris Milson <chris.w.milson@gmail.com>
2021-02-01 17:35:51 +0900

    Implement custom jest matcher
    It checks whether an array obeys the heap invariant according to a given
    compare function.

--- custom-matchers.ts
+++ custom-matchers.ts
@@ -1,26 +1,26 @@
 expect.extend({
-	toObeyHeapInvariant<T>(
-		recieved: T[],
-		compare: (a: T, b: T) => number
-	): jest.CustomMatcherResult {
-		for (let i = 0; i < recieved.length; i++) {
-			const left = 2 * i + 1
-			const right = 2 * i + 2
+  toObeyHeapInvariant<T>(
+    recieved: T[],
+    compare: (a: T, b: T) => number
+  ): jest.CustomMatcherResult {
+    for (let i = 0; i < recieved.length; i++) {
+      const left = 2 * i + 1
+      const right = 2 * i + 2
 
-			if (
-				(left < recieved.length && compare(recieved[i], recieved[left]) > 0) ||
-				(right < recieved.length && compare(recieved[i], recieved[right]) > 0)
-			) {
-				return {
-					pass: false,
-					message: () => `Expected ${recieved} to obey he heap invariant.`,
-				}
-			}
-		}
+      if (
+        (left < recieved.length && compare(recieved[i], recieved[left]) > 0) ||
+        (right < recieved.length && compare(recieved[i], recieved[right]) > 0)
+      ) {
+        return {
+          pass: false,
+          message: () => `Expected ${recieved} to obey he heap invariant.`,
+        }
+      }
+    }
 
-		return {
-			pass: true,
-			message: () => `Expected ${recieved} to not obey the heap invariant.`,
-		}
-	},
+    return {
+      pass: true,
+      message: () => `Expected ${recieved} to not obey the heap invariant.`,
+    }
+  },
 })
