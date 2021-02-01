0db715f89acb1598fc13e737ebc9f2b58b7a393a
Chris Milson <chris.w.milson@gmail.com>
2021-02-01 17:51:34 +0900

    Add heappush and heappop tests

--- src/__tests__/heappush.test.ts
+++ src/__tests__/heappush.test.ts
@@ -1,29 +1,29 @@
 import { Comparator, heappush } from '..'
 
 describe('heappush', () => {
-	// The example heap obeys the heap substructure according to the example
-	// compare function.
-	const exampleHeap = [2, 8, 10, 433, 23111, 23, 31]
-	const exampleCompare: Comparator<number> = (a, b) => a - b
+  // The example heap obeys the heap substructure according to the example
+  // compare function.
+  const exampleHeap = [2, 8, 10, 433, 23111, 23, 31]
+  const exampleCompare: Comparator<number> = (a, b) => a - b
 
-	it('Should add an element to the heap', () => {
-		const heap = [...exampleHeap]
-		const originalLength = heap.length
-		const compare = exampleCompare
-		const value = 10
+  it('Should add an element to the heap', () => {
+    const heap = [...exampleHeap]
+    const originalLength = heap.length
+    const compare = exampleCompare
+    const value = 10
 
-		heappush(heap, value, compare)
+    heappush(heap, value, compare)
 
-		expect(heap.length).toBe(originalLength + 1)
-	})
+    expect(heap.length).toBe(originalLength + 1)
+  })
 
-	it('Should maintain the heap invariant', () => {
-		const heap = [...exampleHeap]
-		const compare = exampleCompare
-		const value = 10
+  it('Should maintain the heap invariant', () => {
+    const heap = [...exampleHeap]
+    const compare = exampleCompare
+    const value = 10
 
-		heappush(heap, value, compare)
+    heappush(heap, value, compare)
 
-		expect(heap).toObeyHeapInvariant(compare)
-	})
+    expect(heap).toObeyHeapInvariant(compare)
+  })
 })
