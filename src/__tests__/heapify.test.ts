26daed84c50885e149467cf6b252d7e3ea2413ad
Chris Milson <chris.w.milson@gmail.com>
2021-02-02 13:42:58 +0900

    Explicitly include custom matcher type
    The custom matcher type is on a global object that should be picked up
    by typescript. This however is not the case. By importing the module, it
    explicitly lets typescript know about our definitions, and we no longer
    get the errors.

--- src/__tests__/heapify.test.ts
+++ src/__tests__/heapify.test.ts
@@ -1,25 +1,26 @@
+import '../typings/jest'
 import { Comparator, heapify } from '..'
 
 describe('heapify', () => {
-	const exampleValues = [8, 2, 10, 433, 23111, 23, 31]
-	const exampleCompare: Comparator<number> = (a, b) => a - b
+  const exampleValues = [8, 2, 10, 433, 23111, 23, 31]
+  const exampleCompare: Comparator<number> = (a, b) => a - b
 
-	it('Should modify an array to obey the heap invariant', () => {
-		const values = [...exampleValues]
-		const compare = exampleCompare
+  it('Should modify an array to obey the heap invariant', () => {
+    const values = [...exampleValues]
+    const compare = exampleCompare
 
-		heapify(values, compare)
+    heapify(values, compare)
 
-		expect(values).toObeyHeapInvariant(compare)
-	})
+    expect(values).toObeyHeapInvariant(compare)
+  })
 
-	it('Should move the minimum of the array to the front', () => {
-		const values = [...exampleValues]
-		const compare = exampleCompare
+  it('Should move the minimum of the array to the front', () => {
+    const values = [...exampleValues]
+    const compare = exampleCompare
 
-		const min = Math.min(...values)
-		heapify(values, compare)
+    const min = Math.min(...values)
+    heapify(values, compare)
 
-		expect(values[0]).toBe(min)
-	})
+    expect(values[0]).toBe(min)
+  })
 })
